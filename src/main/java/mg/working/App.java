66c4042cb07e3eb460e19dcf316d58c57a2ca588
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2025-05-10 21:27:47 +0300

    Initial commit

---
+++ src/main/java/mg/working/App.java
@@ -0,0 +1,25 @@
+package mg.working;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        JFrame fenetre = new JFrame("Application Swing avec Maven");
+        fenetre.setSize(400, 300);
+        fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton bouton = new JButton("Clique-moi !");
+        bouton.addActionListener(e -> JOptionPane.showMessageDialog(null, "Bonjour depuis Swing avec Maven !"));
+
+        fenetre.add(bouton);
+        fenetre.setVisible(true);
+    }
+}
