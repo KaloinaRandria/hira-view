2c44ef46c4ec5b1b1aad0729c1aa6343a77f7616
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2025-05-24 19:29:15 +0300

    Ajout dependance powerpoint

---
+++ src/main/java/mg/working/listener/GenerateButtonListener.java
@@ -0,0 +1,12 @@
+package mg.working.listener;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GenerateButtonListener implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
