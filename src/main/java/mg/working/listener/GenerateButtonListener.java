44da9c45534c87379f5e2c0b18815bcf66f9ab19
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2025-05-24 20:06:16 +0300

    Test background

--- src/main/java/mg/working/listener/GenerateButtonListener.java
+++ src/main/java/mg/working/listener/GenerateButtonListener.java
@@ -1,37 +1,15 @@
 package mg.working.listener;
 
-import org.apache.poi.xslf.usermodel.XMLSlideShow;
-import org.apache.poi.xslf.usermodel.XSLFSlide;
-import org.apache.poi.xslf.usermodel.XSLFTextBox;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.FileOutputStream;
-import java.io.IOException;
+
+import mg.working.powerpoint.PowerpointTraitement;
 
 
 public class GenerateButtonListener implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        String filePath = "test.pptx";
-
-        try (XMLSlideShow ppt = new XMLSlideShow()) {
-            // Création d'une diapositive
-            XSLFSlide slide = ppt.createSlide();
-
-            // Insertion de texte
-            XSLFTextBox textBox = slide.createTextBox();
-            textBox.setText("Bienvenue dans votre présentation PowerPoint !");
-            textBox.setAnchor(new java.awt.Rectangle(50, 50, 400, 100));
-
-            // Sauvegarde dans un fichier
-            try (FileOutputStream out = new FileOutputStream(filePath)) {
-                ppt.write(out);
-                System.out.println("Fichier PowerPoint généré : " + filePath);
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+       new PowerpointTraitement();
     }
 }
---
+++ src/main/java/mg/working/powerpoint/PowerpointTraitement.java
@@ -0,0 +1,64 @@
+package mg.working.powerpoint;
+
+import java.awt.Dimension;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.apache.poi.sl.usermodel.PictureData;
+import org.apache.poi.xslf.usermodel.XMLSlideShow;
+import org.apache.poi.xslf.usermodel.XSLFPictureData;
+import org.apache.poi.xslf.usermodel.XSLFPictureShape;
+import org.apache.poi.xslf.usermodel.XSLFSlide;
+
+public class PowerpointTraitement {
+
+    String filePath;
+
+    public String getFilePath() {
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String dateStr = today.format(formatter);
+        return "fafana_" + dateStr + ".pptx";
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public PowerpointTraitement() {
+        this.filePath = getFilePath();
+
+        try (XMLSlideShow ppt = new XMLSlideShow()) {
+            ppt.setPageSize(new Dimension(1920, 1080));
+
+            // Lire l'image de fond
+            File imageFile = new File("img/background/background.jpg");
+            byte[] pictureData = new FileInputStream(imageFile).readAllBytes();
+
+            // Ajouter l'image à la présentation
+            XSLFPictureData pd = ppt.addPicture(pictureData, PictureData.PictureType.JPEG);
+
+            // Créer une slide
+            XSLFSlide slide = ppt.createSlide();
+
+            // Insérer l'image en fond (plein écran)
+            XSLFPictureShape picture = slide.createPicture(pd);
+            picture.setAnchor(new java.awt.Rectangle(0, 0, 1920, 1080));
+
+            // Sauvegarder le fichier
+            try (FileOutputStream out = new FileOutputStream(filePath)) {
+                ppt.write(out);
+                System.out.println("Présentation créée avec image de fond : " + filePath);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
