6c30957af0625a0d3723cf2c4ce355d8d9adffd7
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2025-05-24 19:46:19 +0300

    ajout images

--- src/main/java/mg/working/App.java
+++ src/main/java/mg/working/App.java
@@ -10,16 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        // JFrame fenetre = new JFrame("Application Swing avec Maven");
-        // fenetre.setSize(400, 300);
-        // fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // JButton bouton = new JButton("Clique-moi !");
-        // bouton.addActionListener(e -> JOptionPane.showMessageDialog(null, "Bonjour depuis Swing avec Maven !"));
-
-        // fenetre.add(bouton);
-        // fenetre.setVisible(true);
-
         new MainWindow();
     }
 }
