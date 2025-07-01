3a11f3cb40623a96f58d9cdb8346b98accd7e83f
Ny Kanto RANDRIAMBOLOLONA <120046452+ny-kanto@users.noreply.github.com>
2025-07-01 20:41:40 +0300

    liste hira brut

--- src/main/java/mg/working/App.java
+++ src/main/java/mg/working/App.java
@@ -1,5 +1,6 @@
 package mg.working;
 
+import mg.working.message.MessageTraitement;
 import mg.working.windows.MainWindow;
 
 /**
@@ -10,6 +11,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        new MainWindow();
+        // new MainWindow();
+        MessageTraitement messageTraitement = new MessageTraitement();
+        String[] hira =  messageTraitement.getHira("SAL.103:1-8.HIR:171/1,3-VVK-S'Aa-FF16/1-VVK-417/2-FKELOKA-487/1-FPINON3-RAHAR-205/2-ANKZ.&T-VVK&S.M-354/3-TORTEN-VVK-547/4-TSDR-RAKITR");
     }
 }
--- src/main/java/mg/working/message/MessageTraitement.java
+++ src/main/java/mg/working/message/MessageTraitement.java
@@ -1,5 +1,21 @@
 package mg.working.message;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MessageTraitement {
-    
+    public String[] getHira(String input) {
+        Pattern pattern = Pattern.compile("(\\d+)/(\\d+)");
+        Matcher matcher = pattern.matcher(input);
+
+        // Utiliser une liste temporaire car on ne connaît pas à l'avance le nombre de matchs
+        java.util.List<String> hiraList = new java.util.ArrayList<>();
+
+        while (matcher.find()) {
+            hiraList.add(matcher.group(1));
+        }
+
+        // Convertir la liste en tableau
+        return hiraList.toArray(new String[0]);
+    }
 }
